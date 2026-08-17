5a9d272b3c65bb0acb1f7414379377005aa0ba80
Aaron Maus <amaus@tulane.edu>
2026-08-17 12:58:21 -0500

    Move tests to test_main

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 from main import *
 
+# 2 pts
 def test_fib_recursive():
     n = 10
     counts = [0] * (n+1)
@@ -7,12 +8,14 @@ def test_fib_recursive():
     print(counts)
     print(sum(counts))
 
+# 2 pts
 def test_fib_top_down():
     n = 10
     fibs = [-1] * (n+1)
     assert fib_top_down(n, fibs) == 55
     print(fibs)
 
+# 2 pts
 def test_fib_bottom_up():
     n = 10
     assert fib_bottom_up(n) == 55
